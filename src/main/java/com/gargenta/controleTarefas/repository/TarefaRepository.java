fc35e4562130a309fca00dbc4c6ded516ea1812e
Guilherme Argenta <guiargenta92@gmail.com>
2025-08-21 15:19:58 -0300

    adicionado o método findByData() no repository

--- src/main/java/com/gargenta/controleTarefas/repository/TarefaRepository.java
+++ src/main/java/com/gargenta/controleTarefas/repository/TarefaRepository.java
@@ -4,6 +4,11 @@ import com.gargenta.controleTarefas.model.Tarefa;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Date;
+import java.util.List;
+
 @Repository
 public interface TarefaRepository extends JpaRepository<Tarefa, Long> {
+
+    List<Tarefa> findByData(Date data);
 }
