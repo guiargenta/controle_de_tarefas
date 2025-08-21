f77b6466fe1bf2f1ca27b3bfc478cc39500c5883
Guilherme Argenta <guiargenta92@gmail.com>
2025-08-21 15:21:51 -0300

    adicionado o método buscarTarefaPorData() no @controller

--- src/main/java/com/gargenta/controleTarefas/controller/TarefaController.java
+++ src/main/java/com/gargenta/controleTarefas/controller/TarefaController.java
@@ -2,11 +2,12 @@ package com.gargenta.controleTarefas.controller;
 
 import com.gargenta.controleTarefas.model.Tarefa;
 import com.gargenta.controleTarefas.service.TarefaService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Date;
+import java.util.List;
 
 @RestController
 @RequestMapping("/tarefa")
@@ -21,8 +22,13 @@ public class TarefaController {
     @PostMapping()
     public ResponseEntity<Void> salvarTarefa(@RequestBody Tarefa tarefa) {
         tarefaService.salvarTarefa(tarefa);
-        return ResponseEntity.ok().build();
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
+    @GetMapping
+    public ResponseEntity<List<Tarefa>> buscarTarefaPorData(@RequestParam Date data) {
+        List<Tarefa> tarefas = tarefaService.buscarTarefaPorData(data);
+        return ResponseEntity.status(HttpStatus.OK).body(tarefas);
+    }
 
 }
