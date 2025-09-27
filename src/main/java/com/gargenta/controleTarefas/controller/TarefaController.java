c165510e4d315f5135a27b30affd87d695cf2c8c
Guilherme Argenta <guiargenta92@gmail.com>
2025-09-27 20:05:59 -0300

    ajustada documentação no Swagger  para todos os métodos no TarefaController

--- src/main/java/com/gargenta/controleTarefas/controller/TarefaController.java
+++ src/main/java/com/gargenta/controleTarefas/controller/TarefaController.java
@@ -42,6 +42,15 @@ public class TarefaController {
         return ResponseEntity.status(HttpStatus.CREATED).body(TarefaMapper.toDto(tarefa));
     }
 
+    @Operation(summary = "Incrementar quantidade de uma tarefa", description = "Recurso para incrementar quantidade de tarefa cumprida com nome já existente",
+            responses = {
+                    @ApiResponse(responseCode = "201", description = "Incrementada com sucesso",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = TarefaResponseDto.class))),
+                    @ApiResponse(responseCode = "400", description = "Não foi possível incrementar",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class))),
+                    @ApiResponse(responseCode = "404", description = "Recurso não encontrado",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class)))
+            })
     @PostMapping("/incrementar")
     public ResponseEntity<Tarefa> incrementarQuantidade(
             @RequestParam String nomeTarefa,
@@ -51,11 +60,25 @@ public class TarefaController {
         return ResponseEntity.ok(tarefaService.incrementarQuantidade(nomeTarefa, dataTarefa, quantidade));
     }
 
+    @Operation(summary = "Tarefas cumpridas em um dia específico", description = "Recursos para listar as tarefas cumpridas em um dia determinados",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Tarefas localizadas com sucesso",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = TarefaResponseDto.class))),
+                    @ApiResponse(responseCode = "404", description = "Nenhuma tarefa foi localizada",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class)))
+            })
     @GetMapping("/data/{data}")
     public ResponseEntity<List<Tarefa>> buscarTarefaCumpridaPorData(@RequestParam LocalDate data) {
         return ResponseEntity.ok(tarefaService.buscarTarefaPorData(data));
     }
 
+    @Operation(summary = "Listar tarefas cumprida entre datas", description = "Recurso para listar as tarefas cumpridas entre datas determninadas",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Tarefas listadas com sucesso",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = TarefaResponseDto.class))),
+                    @ApiResponse(responseCode = "404", description = "Nenhuma tarefa localizada nesse período de datas",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class)))
+            })
     @GetMapping("/periodo")
     public ResponseEntity<List<Tarefa>> buscarTarefaCumpridaEntreDatas(
             @RequestParam @DateTimeFormat(pattern = "dd-MM-yyyy") LocalDate dataInicio,
@@ -63,6 +86,13 @@ public class TarefaController {
         return ResponseEntity.ok(tarefaService.buscarTarefaEntreDatas(dataInicio, dataFim));
     }
 
+    @Operation(summary = "Listar todas as tarefas cumpridas", description = "Recurso que recupera todas as tarefas cumpridas registradas",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Tarefas cumpridas listadas com sucesso",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = TarefaResponseDto.class))),
+                    @ApiResponse(responseCode = "400", description = "Não foi possível recuperar as tarefas",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class)))
+            })
     @GetMapping
     public ResponseEntity<List<TarefaResponseDto>> buscarTodas() {
         List<Tarefa> tarefas = tarefaService.buscarTodas();
@@ -70,6 +100,13 @@ public class TarefaController {
         return ResponseEntity.ok(dtos);
     }
 
+    @Operation(summary = "Número total de tarefas cumpridas hoje", description = "Recurso que cita o número total de tarefas cumpridas no dia de hoje",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Quantidade listada com sucesso",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = TarefaResponseDto.class))),
+                    @ApiResponse(responseCode = "404", description = "Não foi possível completar a requisição, quantidade não encontrada",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class)))
+            })
     @GetMapping("/total-hoje")
     public ResponseEntity<Integer> quantidadeTotalHoje() {
         return ResponseEntity.ok(tarefaService.quantidadeTotalPorData(LocalDate.now()));
