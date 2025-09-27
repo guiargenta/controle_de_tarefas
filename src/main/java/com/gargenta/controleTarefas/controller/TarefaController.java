4b936714eb6e34371b4ec9e2353aeafa3f1d0e62
Guilherme Argenta <guiargenta92@gmail.com>
2025-09-27 19:18:14 -0300

    ajustada versão no pom.xlm do OpenApi para ser compatível com o Spring 6.2, e iniciada a implementação da documentação no Swagger do @TarefaController

--- src/main/java/com/gargenta/controleTarefas/controller/TarefaController.java
+++ src/main/java/com/gargenta/controleTarefas/controller/TarefaController.java
@@ -2,8 +2,14 @@ package com.gargenta.controleTarefas.controller;
 
 import com.gargenta.controleTarefas.dto.TarefaResponseDto;
 import com.gargenta.controleTarefas.dto.mapper.TarefaMapper;
+import com.gargenta.controleTarefas.exception.exceptionHandler.ErrorMessage;
 import com.gargenta.controleTarefas.model.Tarefa;
 import com.gargenta.controleTarefas.service.TarefaService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +18,7 @@ import org.springframework.web.bind.annotation.*;
 import java.time.LocalDate;
 import java.util.List;
 
+@Tag(name = "Tarefas", description = "Recursos para criar, alterar, consultar e deletar tarefas.")
 @RestController
 @RequestMapping("/api/tarefas")
 public class TarefaController {
@@ -22,6 +29,13 @@ public class TarefaController {
         this.tarefaService = tarefaService;
     }
 
+    @Operation(summary = "Criar ou atualizar tarefa cumprida.", description = "Recurso para criar uma nova tarefa que foi cumprida ou alterar uma que já exista.",
+            responses = {
+                    @ApiResponse(responseCode = "201", description = "Tarefa criada com sucesso",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = TarefaResponseDto.class))),
+                    @ApiResponse(responseCode = "409", description = "Conflito ao criar tarefa",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class)))
+            })
     @PostMapping()
     public ResponseEntity<TarefaResponseDto> criarOuAtualizarTarefaCumprida(@RequestBody TarefaResponseDto dto) {
         Tarefa tarefa = tarefaService.salvarTarefaOuAtualizarTarefa(TarefaMapper.toTarefa(dto));
