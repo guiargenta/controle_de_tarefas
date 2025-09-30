38154404dfb449c678d309ae0c147ee7d0e2c8fd
Guilherme Argenta <guiargenta92@gmail.com>
2025-09-30 11:04:04 -0300

    implementado teste para tentativa de criar usuário com password inválido (em branco ou fora do tamanho permitido).

--- src/test/java/com/gargenta/controleTarefas/UsuarioIT.java
+++ src/test/java/com/gargenta/controleTarefas/UsuarioIT.java
@@ -2,6 +2,8 @@ package com.gargenta.controleTarefas;
 
 import com.gargenta.controleTarefas.dto.CreateUsuarioDto;
 import com.gargenta.controleTarefas.dto.UsuarioResponseDto;
+import com.gargenta.controleTarefas.exception.exceptionHandler.ErrorMessage;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -30,7 +32,37 @@ public class UsuarioIT {
                 .returnResult().getResponseBody();
 
         org.assertj.core.api.Assertions.assertThat(responseBody).isNotNull();
+        org.assertj.core.api.Assertions.assertThat(responseBody.getRole().equalsIgnoreCase("ROLE_USER"));
         org.assertj.core.api.Assertions.assertThat(responseBody.getUsername().equalsIgnoreCase("fulano"));
         org.assertj.core.api.Assertions.assertThat(responseBody.getEmail().equalsIgnoreCase("fulano@gmail.com"));
+    }
+
+    @Test
+    public void criarUsuario_comPasswordInvalido_retornarUsuarioCriadoComStatus422() {
+        ErrorMessage responseBody = webTestClient
+                .post()
+                .uri("/api/usuarios")
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(new CreateUsuarioDto("fulano@gmail.com", "12345", "fulano"))
+                .exchange()
+                .expectStatus().isEqualTo(422)
+                .expectBody(ErrorMessage.class)
+                .returnResult().getResponseBody();
+
+        Assertions.assertNotNull(responseBody);
+        org.assertj.core.api.Assertions.assertThat(responseBody.getStatus()).isEqualTo(422);
+
+        responseBody = webTestClient
+                .post()
+                .uri("/api/usuarios")
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(new CreateUsuarioDto("fulano@gmail.com", "", "fulano"))
+                .exchange()
+                .expectStatus().isEqualTo(422)
+                .expectBody(ErrorMessage.class)
+                .returnResult().getResponseBody();
+
+        Assertions.assertNotNull(responseBody);
+        org.assertj.core.api.Assertions.assertThat(responseBody.getStatus()).isEqualTo(422);
     }
 }
