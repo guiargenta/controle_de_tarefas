495f867e7aebe5dbe7c9b14d1aa0586a963a01a9
Guilherme Argenta <guiargenta92@gmail.com>
2025-12-11 18:58:06 -0300

    fix(auth): quando estiver autenticado e não houver autorização, retornar 403 (forbidden)

--- src/main/java/com/gargenta/controleTarefas/exception/exceptionHandler/ApiExceptionHandler.java
+++ src/main/java/com/gargenta/controleTarefas/exception/exceptionHandler/ApiExceptionHandler.java
@@ -9,6 +9,7 @@ import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -17,6 +18,21 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @Slf4j
 @RestControllerAdvice
 public class ApiExceptionHandler {
+
+    @ExceptionHandler(org.springframework.security.access.AccessDeniedException.class)
+    public ResponseEntity<ErrorMessage> accessDeniedException(
+            AccessDeniedException ex, HttpServletRequest request) {
+
+        log.error("Api Error: " + ex);
+        return ResponseEntity
+                .status(HttpStatus.FORBIDDEN)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(new ErrorMessage(
+                        request,
+                        HttpStatus.FORBIDDEN,
+                        ex.getMessage()
+                ));
+    }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ErrorMessage> methodArgumentNotValidException(
