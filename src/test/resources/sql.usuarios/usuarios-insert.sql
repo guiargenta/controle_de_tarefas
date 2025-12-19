9f65d956ef7da4efb4a5af9a1d6ca5e6f07ac1aa
Guilherme Argenta <guiargenta92@gmail.com>
2025-12-18 21:38:09 -0300

    fix(test): senha criptografada para os usuarios no banco

--- src/test/resources/sql.usuarios/usuarios-insert.sql
+++ src/test/resources/sql.usuarios/usuarios-insert.sql
@@ -1,8 +1,10 @@
 INSERT INTO usuarios (id, username, email, password, role)
     VALUES (100, 'teste1', 'teste1@gmail.com', '$2a$12$tjgpKMmUuLL0Kc9Ai723POHmdYJQhYLt1Acd0/LOAOrsM7lIFx/22', 'ROLE_ADMIN');
 
+
 INSERT INTO usuarios (id, username, email, password, role)
     VALUES (101, 'teste2', 'teste2@gmail.com', '$2a$12$Es4WFuD4E1TLNMvQZwgknO/075oVTAJB5txZLoNk79FOYLMsODOQ2', 'ROLE_USER');
+
 
 INSERT INTO usuarios (id, username, email, password, role)
     VALUES (102, 'teste3', 'teste3@gmail.com', '$2a$12$W4ayJWN2ohnxzyqx7AA7beddl7wKsbHKGDpHbPvzKwbyR.XjsKIOq', 'ROLE_USER');
